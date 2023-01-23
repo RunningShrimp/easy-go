bbe9301036aa90e8e1b888dd0f7e257e276e144e
Runningshrimp <runningshrimp@163.com>
2023-01-23 12:14:30 +0800

    feat：增加example

--- app/http.go
+++ app/http.go
@@ -3,36 +3,36 @@ package app
 import "net/http"
 
 type IController interface {
-	InitController(ctx EasyGoContext)
+	InitController(ctx *EasyGoContext)
 }
 type IRequest interface {
-	InitRequest(ctx EasyGoContext)
+	InitRequest(ctx *EasyGoContext)
 }
 
 // RequestMid 请求中间件，所有请求中间件都应该实现这个函数
-type RequestMid func(req *http.Request)
+type RequestMid func(*http.Request)
 
 // ResponseMid 响应中间件，所有的响应中间都应该实现这个函数
-type ResponseMid func(resp *http.Response)
+type ResponseMid func(*http.Response)
 
 // RequestHandler 请求处理的handler
-type RequestHandler func(request IRequest) string
+type RequestHandler func(IRequest) string
 
 // BaseController 基础的Controller，
 // 所有的实现自动注册的rest风格的api都应该继承BaseController
 type BaseController struct {
-	Ctx EasyGoContext
+	Ctx *EasyGoContext
 }
 
 // BaseRequest 基础的request参数，所有的请求处理handler的参数都应该继承BaseRequest
 type BaseRequest struct {
-	Ctx EasyGoContext
+	Ctx *EasyGoContext
 }
 
-func (c *BaseController) InitController(ctx EasyGoContext) {
+func (c *BaseController) InitController(ctx *EasyGoContext) {
 	c.Ctx = ctx
 }
 
-func (r *BaseRequest) InitRequest(ctx EasyGoContext) {
+func (r *BaseRequest) InitRequest(ctx *EasyGoContext) {
 	r.Ctx = ctx
 }
